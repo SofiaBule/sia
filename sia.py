c157bb3fb430a799249fd66fc9e99f47f64e25b7
SofiaBule <146926716+SofiaBule@users.noreply.github.com>
2024-02-29 20:15:05 +0100

    Add files via upload

---
+++ sia.py
@@ -0,0 +1,6 @@
+# Script by Ivan Petrovich
+def simulate_russia_traffic_count():
+    return random.randint(60000000, 80000000)  # Simulating traffic count in Russia within a range
+
+russia_traffic_count = simulate_russia_traffic_count()
+print("Simulated traffic count in Russia:", russia_traffic_count)
